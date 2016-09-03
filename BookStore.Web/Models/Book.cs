ccbd380d89389f3203f37b48952aae63f1f45a26
jasonbai2014 <jasonjason20062006@yahoo.com>
2016-09-02 18:45:38 -0700

    added book and review model class

---
+++ BookStore.Web/Models/Book.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Web.Models
+{
+    /// <summary>
+    /// This is a model class for books
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// This is book id
+        /// </summary>
+        public int BookID { get; set; }
+
+        /// <summary>
+        /// This is an url pointing to a location on the server where cover
+        /// image of the book is kept
+        /// </summary>
+        public String CoverUrl { get; set; }
+
+        /// <summary>
+        /// This is book name
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// This is book description
+        /// </summary>
+        public String Description { get; set; }
+
+        /// <summary>
+        /// This is author name
+        /// </summary>
+        public String Author { get; set; }
+
+        /// <summary>
+        /// This is book price
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// This is rating of the book
+        /// </summary>
+        public float Rating { get; set; }
+
+        /// <summary>
+        /// This is number of reviews for the book
+        /// </summary>
+        public int TotalReviews { get; set; }
+
+        /// <summary>
+        /// This is total pages of the book
+        /// </summary>
+        public int pages { get; set; }
+
+        /// <summary>
+        /// This is publisher of the book
+        /// </summary>
+        public String Publisher { get; set; }
+
+        /// <summary>
+        /// This is ISBN of the book
+        /// </summary>
+        public String ISBN { get; set; }
+
+        /// <summary>
+        /// This is a list of reviews for the book
+        /// </summary>
+        public virtual ICollection<Review> Reviews { get; set; }
+
+    }
+}
---
+++ BookStore.Web/Models/Review.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Web.Models
+{
+    /// <summary>
+    /// This is a model for a book review
+    /// </summary>
+    public class Review
+    {
+        /// <summary>
+        /// This is review id
+        /// </summary>
+        public int ReviewID { get; set; }
+
+        /// <summary>
+        /// This is an id of a book for which this review is written
+        /// </summary>
+        public int BookID { get; set; }
+
+        /// <summary>
+        /// This is reviewer name
+        /// </summary>
+        public String Reviewer { get; set; }
+
+        /// <summary>
+        /// This is a rating 
+        /// </summary>
+        public int Rating { get; set; }
+
+        /// <summary>
+        /// This is review text
+        /// </summary>
+        public String ReviewText { get; set; }
+
+        /// <summary>
+        /// This is the date this review is posted
+        /// </summary>
+        [DataType(DataType.Date)]
+        public DateTime PostDate { get; set; }
+
+        /// <summary>
+        /// This is the book this review belongs to
+        /// </summary>
+        public virtual Book Book { get; set; }
+    }
+}
